f64934a7d9307a95e8fc63a653f09e31602e0916
ricky <ricky@cloudprodigy.ca>
2023-08-18 10:28:01 -0400

    drop down

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # drop down list
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
