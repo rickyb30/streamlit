340e221e7a23e86f1579c99b263f9842d1668b3c
ricky <ricky@cloudprodigy.ca>
2023-09-13 08:03:22 -0400

    added snowflake conn

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
         streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
